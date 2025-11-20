e94ab40374a6d1f8297a16e3ac0d33e39c0a64bb
Nick <Kiku.Reise@gmail.com>
2025-11-20 20:10:29 +0200

    chore: temporarily disable caching to force cache clear

--- kikuai-site-nuxt/nuxt.config.ts
+++ kikuai-site-nuxt/nuxt.config.ts
@@ -69,7 +69,7 @@ export default defineNuxtConfig({
     routeRules: {
       '/**': {
         headers: {
-          'cache-control': 'public, max-age=0, s-maxage=1800'
+          'cache-control': 'no-cache, no-store, must-revalidate'
         }
       },
       '/assets/**': {
